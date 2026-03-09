46f3b49c89936eb44a1748b0235e4bf3ef43911b
Suvorov.Alexandr <sasha.suvorov03@gmail.com>
2026-03-09 14:33:01 +1200

    task 14.2 done

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     name: str
     description: str
@@ -9,7 +12,23 @@ class Category:
     def __init__(self, name, description, product):
         self.name = name
         self.description = description
-        self.products = product
+        self.__products = product
 
         Category.category_count += 1
         Category.product_count += len(product)
+
+    def add_product(self, new_product: Product):
+        self.__products.append(new_product)
+        self.product_count += 1
+
+    @property
+    def products(self):
+        prod_list = ""
+        for product in self.__products:
+            prod_list += f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n'
+        return prod_list
+
+
+
+
+
