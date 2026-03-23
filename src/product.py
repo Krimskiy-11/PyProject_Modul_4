42599564074e28f7ef62ac7796ae6c92c9f6a662
Suvorov.Alexandr <sasha.suvorov03@gmail.com>
2026-03-23 14:30:03 +1200

    task 17.1 done

--- src/product.py
+++ src/product.py
@@ -37,6 +37,8 @@ class Product(BaseProduct, MixinLog):
         self.name = name
         self.description = description
         self.__price = price
+        if quantity <= 0:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен")
         self.quantity = quantity
         super().__init__(name, description, price, quantity)
         MixinLog.__init__(self)
